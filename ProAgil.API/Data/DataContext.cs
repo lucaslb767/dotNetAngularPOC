d907d78a61f033561f2a58da1dc0e9ea129444ef
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-10-01 15:34:18 -0300

    initial commit

---
+++ ProAgil.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ProAgil.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+    }
+}
